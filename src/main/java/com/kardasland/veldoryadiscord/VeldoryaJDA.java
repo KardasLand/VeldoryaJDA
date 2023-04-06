c18b4a64bc3f50094edc45bc1bc1e37b5a616241
Anıl <kardasland@hotmail.com>
2023-04-06 17:27:05 +0300

    Added some aliases for the commands.

--- src/main/java/com/kardasland/veldoryadiscord/VeldoryaJDA.java
+++ src/main/java/com/kardasland/veldoryadiscord/VeldoryaJDA.java
@@ -74,7 +74,7 @@ public final class VeldoryaJDA extends JavaPlugin {
      * 2023 for me is YKS (Uni entrance exam) year, so I don't have proper mental for cleaning and optimizing codes.
      * Because of this, my code quality should be trash xd
      * But you should not have a hard time reading lines.
-     * I'm not giving support for this plugin. I do not have time for this.
+     * I'm not giving support for this plugin. I do not have enough time for this.
      * But if you want to read/modify it but can't read it or too messy, or if you want to commission me after June 2023, you can contact me.
      * - KardasLand#9552
      */
